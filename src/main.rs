2761a728d753308a5335bcc34235f5e2af68d9fa
leozhang <it@tparts.com>
2024-07-14 17:36:22 +0800

    feat: hello world
    Signed-off-by: leozhang <it@tparts.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    greet_world();
+}
+
+fn greet_world() {
+    let southern_germany = "Grüß Gott!";
+    let chinese = "世界，你好";
+    let english = "World, hello";
+    let regions = [southern_germany, chinese, english];
+    for region in regions.iter() {
+        println!("{}", &region)
+    }
 }
